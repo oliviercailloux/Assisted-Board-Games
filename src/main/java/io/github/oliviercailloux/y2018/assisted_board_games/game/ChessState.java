b5fc09612a56141a00d2371bdefac21eab00973a
ThibaudduSartel <t.dusartel@hotmail.fr>
2019-02-15 15:14:28 +0100

    encoding and decoding state in json object

---
+++ src/main/java/io/github/oliviercailloux/y2018/assisted_board_games/game/ChessState.java
@@ -0,0 +1,196 @@
+package io.github.oliviercailloux.y2018.assisted_board_games.game;
+
+import javax.json.*;
+
+import com.github.bhlangonijr.chesslib.Board;
+import com.github.bhlangonijr.chesslib.Piece;
+import com.github.bhlangonijr.chesslib.Square;
+
+/***
+ * 
+ * @author Thibaud Fremin du Sartel
+ *
+ */
+public class ChessState {
+	
+	public static JsonObject encodeState(Board board) {
+		Piece pieces[] = board.boardToArray();
+		JsonObjectBuilder obj = Json.createObjectBuilder();
+		
+		for (int i=0; i<pieces.length; i++) {
+			Piece p = pieces[i];
+//			if(!Square.values()[i].value().equals("NONE")) {
+//				obj.add(Square.values()[i].value(), p.value());
+//				
+//			}
+			
+			obj.add(Integer.toString(i), p.value());
+			
+		}
+		obj.add("player", board.getSideToMove().value());
+		
+		return obj.build();
+	}
+	
+	public static Board decodeFromJson(JsonObject json) {
+		Board board = new Board();
+		Piece pieces[] = new Piece[64];
+		String player = json.getString("player");
+		String fenString = "";
+		int count = 0;
+		
+		// write the first part of the FEN String, containing the places of the pieces on the board
+		for(int i=0; i<pieces.length; i++) {
+			if(i != 0 && i%8 == 0) {
+				fenString += "/";
+			}
+			String currentPiece = json.getString(Integer.toString(i));
+			
+			switch (currentPiece) {
+			case "WHITE_ROOK":
+				if(count != 0){
+					fenString += Integer.toString(count);
+					count = 0;
+				}
+				fenString += "r";
+				break;
+				
+			case "BLACK_ROOK":
+				if(count != 0){
+					fenString += Integer.toString(count);
+					count = 0;
+				}
+				fenString += "R";
+				break;
+				
+			case "WHITE_KNIGHT":
+				if(count != 0){
+					fenString += Integer.toString(count);
+					count = 0;
+				}
+				fenString += "n";
+				break;
+				
+			case "BLACK_KNIGHT":
+				if(count != 0){
+					fenString += Integer.toString(count);
+					count = 0;
+				}
+				fenString += "N";
+				break;
+				
+			case "BLACK_BISHOP":
+				if(count != 0){
+					fenString += Integer.toString(count);
+					count = 0;
+				}
+				fenString += "B";
+				break;
+				
+			case "WHITE_BISHOP":
+				if(count != 0){
+					fenString += Integer.toString(count);
+					count = 0;
+				}
+				fenString += "b";
+				break;
+				
+			case "WHITE_QUEEN":
+				if(count != 0){
+					fenString += Integer.toString(count);
+					count = 0;
+				}
+				fenString += "q";
+				break;
+				
+			case "BLACK_QUEEN":
+				if(count != 0){
+					fenString += Integer.toString(count);
+					count = 0;
+				}
+				fenString += "Q";
+				break;
+				
+			case "WHITE_KING":
+				if(count != 0){
+					fenString += Integer.toString(count);
+					count = 0;
+				}
+				fenString += "k";
+				break;
+				
+			case "BLACK_KING":
+				if(count != 0){
+					fenString += Integer.toString(count);
+					count = 0;
+				}
+				fenString += "K";
+				break;
+				
+			case "WHITE_PAWN":
+				if(count != 0){
+					fenString += Integer.toString(count);
+					count = 0;
+				}
+				fenString += "p";
+				break;
+				
+			case "BLACK_PAWN":
+				if(count != 0){
+					fenString += Integer.toString(count);
+					count = 0;
+				}
+				fenString += "P";
+				break;
+				
+			case "NONE":
+				count ++;
+				break;
+
+			default:
+				break;
+			} 
+			
+			if((i+1)%8 == 0) {
+				// last square of the line : add count's value to fenString if not = 0 
+				// 							 set count to zero
+				if(count != 0) {
+					fenString += Integer.toString(count);
+					count = 0;
+				}
+			}
+				
+			 
+		}
+		
+		//add the player who has to play next
+		if(player.equals("WHITE")) {
+			fenString += " w";
+		}else {
+			fenString += " b";
+		}
+		
+		//add the rest set to "-"
+		fenString += " - - 0 1";
+		
+		board.loadFromFen(fenString);
+		
+		return board;
+	}
+	
+	public static void main(String args[]) {
+		Board board = new Board();
+//		Piece[] p = board.boardToArray();
+//		for(int i = 0; i<p.length; i++) {
+//			System.out.println(" " + p[i]);
+//		}
+		
+		System.out.println(board);
+		JsonObject json = encodeState(board);
+		System.out.println(json);
+		
+		System.out.println(decodeFromJson(json));
+		
+	}
+	
+}
