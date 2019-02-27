f62da5121179d6ec929d3e3787d6be250cceac83
ThibaudduSartel <t.dusartel@hotmail.fr>
2019-02-27 23:32:27 +0100

    remove pretty printing

--- src/test/java/io/github/oliviercailloux/y2018/assisted_board_games/game/ChessStateTest.java
+++ src/test/java/io/github/oliviercailloux/y2018/assisted_board_games/game/ChessStateTest.java
@@ -10,12 +10,12 @@ import javax.json.JsonObject;
 
 
 import org.apache.commons.io.IOUtils;
-
+//import org.eclipse.persistence.jpa.jpql.tools.model.EclipseLinkConditionalStateObjectBuilder;
 import org.junit.jupiter.api.Test;
 
 import com.github.bhlangonijr.chesslib.Board;
 import io.github.oliviercailloux.y2018.assisted_board_games.game.ChessState;
-import io.github.oliviercailloux.y2018.assisted_board_games.ressources.utils.JsonHelper;
+//import io.github.oliviercailloux.y2018.assisted_board_games.ressources.utils.JsonHelper;
 
 /***
  * @author Thibaud Fremin du Sartel
@@ -27,89 +27,19 @@ class ChessStateTest {
 
 	@Test
 	public void testEncodeState() throws IOException, FileNotFoundException {
-		JsonHelper jsonHelper = new JsonHelper();
+//		JsonHelper jsonHelper = new JsonHelper();
 		Board board = new Board();
 		JsonObject encodedBoard = ChessState.encodeState(board);
-		String result = jsonHelper.asPrettyString(encodedBoard);
-		
-		String result2 = "\r\n" + 
-				"{\r\n" + 
-				"    \"0\": \"WHITE_ROOK\",\r\n" + 
-				"    \"1\": \"WHITE_KNIGHT\",\r\n" + 
-				"    \"2\": \"WHITE_BISHOP\",\r\n" + 
-				"    \"3\": \"WHITE_QUEEN\",\r\n" + 
-				"    \"4\": \"WHITE_KING\",\r\n" + 
-				"    \"5\": \"WHITE_BISHOP\",\r\n" + 
-				"    \"6\": \"WHITE_KNIGHT\",\r\n" + 
-				"    \"7\": \"WHITE_ROOK\",\r\n" + 
-				"    \"8\": \"WHITE_PAWN\",\r\n" + 
-				"    \"9\": \"WHITE_PAWN\",\r\n" + 
-				"    \"10\": \"WHITE_PAWN\",\r\n" + 
-				"    \"11\": \"WHITE_PAWN\",\r\n" + 
-				"    \"12\": \"WHITE_PAWN\",\r\n" + 
-				"    \"13\": \"WHITE_PAWN\",\r\n" + 
-				"    \"14\": \"WHITE_PAWN\",\r\n" + 
-				"    \"15\": \"WHITE_PAWN\",\r\n" + 
-				"    \"16\": \"NONE\",\r\n" + 
-				"    \"17\": \"NONE\",\r\n" + 
-				"    \"18\": \"NONE\",\r\n" + 
-				"    \"19\": \"NONE\",\r\n" + 
-				"    \"20\": \"NONE\",\r\n" + 
-				"    \"21\": \"NONE\",\r\n" + 
-				"    \"22\": \"NONE\",\r\n" + 
-				"    \"23\": \"NONE\",\r\n" + 
-				"    \"24\": \"NONE\",\r\n" + 
-				"    \"25\": \"NONE\",\r\n" + 
-				"    \"26\": \"NONE\",\r\n" + 
-				"    \"27\": \"NONE\",\r\n" + 
-				"    \"28\": \"NONE\",\r\n" + 
-				"    \"29\": \"NONE\",\r\n" + 
-				"    \"30\": \"NONE\",\r\n" + 
-				"    \"31\": \"NONE\",\r\n" + 
-				"    \"32\": \"NONE\",\r\n" + 
-				"    \"33\": \"NONE\",\r\n" + 
-				"    \"34\": \"NONE\",\r\n" + 
-				"    \"35\": \"NONE\",\r\n" + 
-				"    \"36\": \"NONE\",\r\n" + 
-				"    \"37\": \"NONE\",\r\n" + 
-				"    \"38\": \"NONE\",\r\n" + 
-				"    \"39\": \"NONE\",\r\n" + 
-				"    \"40\": \"NONE\",\r\n" + 
-				"    \"41\": \"NONE\",\r\n" + 
-				"    \"42\": \"NONE\",\r\n" + 
-				"    \"43\": \"NONE\",\r\n" + 
-				"    \"44\": \"NONE\",\r\n" + 
-				"    \"45\": \"NONE\",\r\n" + 
-				"    \"46\": \"NONE\",\r\n" + 
-				"    \"47\": \"NONE\",\r\n" + 
-				"    \"48\": \"BLACK_PAWN\",\r\n" + 
-				"    \"49\": \"BLACK_PAWN\",\r\n" + 
-				"    \"50\": \"BLACK_PAWN\",\r\n" + 
-				"    \"51\": \"BLACK_PAWN\",\r\n" + 
-				"    \"52\": \"BLACK_PAWN\",\r\n" + 
-				"    \"53\": \"BLACK_PAWN\",\r\n" + 
-				"    \"54\": \"BLACK_PAWN\",\r\n" + 
-				"    \"55\": \"BLACK_PAWN\",\r\n" + 
-				"    \"56\": \"BLACK_ROOK\",\r\n" + 
-				"    \"57\": \"BLACK_KNIGHT\",\r\n" + 
-				"    \"58\": \"BLACK_BISHOP\",\r\n" + 
-				"    \"59\": \"BLACK_QUEEN\",\r\n" + 
-				"    \"60\": \"BLACK_KING\",\r\n" + 
-				"    \"61\": \"BLACK_BISHOP\",\r\n" + 
-				"    \"62\": \"BLACK_KNIGHT\",\r\n" + 
-				"    \"63\": \"BLACK_ROOK\",\r\n" + 
-				"    \"64\": \"NONE\",\r\n" + 
-				"    \"player\": \"WHITE\"\r\n" + 
-				"}";
+//		String result = jsonHelper.asPrettyString(encodedBoard);
 
 		FileInputStream fis = new FileInputStream(fileTest);
 		String expectedResult = IOUtils.toString(fis, "UTF-8");
 		
-		System.out.println("expected:\n" + expectedResult);
-		System.out.println("result:\n" + result);
-		System.out.println("result2:\n" + result2);
+//		System.out.println("expected:\n" + expectedResult);
+//		System.out.println("result:\n" + result);
+//		System.out.println("result2:\n" + result2);
 		
-		assertEquals(expectedResult, result);
+		assertEquals(expectedResult, encodedBoard.toString());
 	}
 
 	@Test
