c1609b6c66b2eea700627d5ee9149c3f7864a554
Douo Bougna Delmas <douobougna@gmail.com>
2019-03-19 20:33:04 +0100

    add QueryHelper class and service class

---
+++ src/main/java/io/github/oliviercailloux/y2018/assisted_board_games/ressources/service/ChessService.java
@@ -0,0 +1,66 @@
+package io.github.oliviercailloux.y2018.assisted_board_games.ressources.service;
+
+
+import io.github.oliviercailloux.y2018.assisted_board_games.ressources.utils.QueryHelper;
+
+
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import io.github.oliviercailloux.y2018.assisted_board_games.model.*;
+
+
+@RequestScoped
+public class ChessService {
+	/***
+	 * 
+	 * @author Delmas Douo Bougna
+	 *
+	 */
+	@PersistenceContext
+	private EntityManager em;
+
+	@Inject
+	private QueryHelper helper;
+	
+	@Transactional
+	public List<ChessGameEntity> getAllGame() {
+
+		return em.createQuery(helper.selectAll(ChessGameEntity.class)).getResultList();
+
+	}
+	
+	@Transactional
+	public List<ChessStateEntity> getAllState() {
+
+		return em.createQuery(helper.selectAll(ChessStateEntity.class)).getResultList();
+
+	}
+	
+	@Transactional
+	public List<ChessMoveEntity> getAllMove() {
+
+		return em.createQuery(helper.selectAll(ChessMoveEntity.class)).getResultList();
+
+	}
+	
+	@Transactional
+	public void persist(ChessGameEntity game) {
+		em.persist(game);
+	}
+	@Transactional
+	public void persist(ChessStateEntity state) {
+		em.persist(state);
+	}
+	
+	@Transactional
+	public void persist(ChessMoveEntity move) {
+		em.persist(move);
+	}
+
+}
---
+++ src/main/java/io/github/oliviercailloux/y2018/assisted_board_games/ressources/utils/QueryHelper.java
@@ -0,0 +1,40 @@
+package io.github.oliviercailloux.y2018.assisted_board_games.ressources.utils;
+
+import static java.util.Objects.requireNonNull;
+
+import javax.enterprise.context.RequestScoped;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+@RequestScoped
+public class QueryHelper {
+	/***
+	 * Code From https://github.com/oliviercailloux/samples/blob/master/JavaEE-JPA-Inject-Servlets/src/main/java/io/github/oliviercailloux/javaee_jpa_inject_servlets/utils/QueryHelper.java
+	 * @Author : Olivier Cailloux
+	 */
+	
+	@PersistenceUnit
+	private EntityManagerFactory emFactory;
+
+	public QueryHelper() {
+		emFactory = null;
+	}
+
+	public EntityManagerFactory getEmFactory() {
+		return requireNonNull(emFactory);
+	}
+
+	public <T> CriteriaQuery<T> selectAll(Class<T> type) {
+		final CriteriaQuery<T> query = emFactory.getCriteriaBuilder().createQuery(type);
+		final Root<T> from = query.from(type);
+		query.select(from);
+		return query;
+	}
+
+	public void setEmFactory(EntityManagerFactory emFactory) {
+		this.emFactory = requireNonNull(emFactory);
+	}
+
+}
