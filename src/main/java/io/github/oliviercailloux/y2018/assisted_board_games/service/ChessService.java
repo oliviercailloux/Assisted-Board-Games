2b2c7653100d194bdc6f96bab7ee27a3cf446dd8
MeganBr <megan.brassard@gmail.com>
2019-04-06 11:58:11 +0200

    clean back

--- src/main/java/io/github/oliviercailloux/y2018/assisted_board_games/service/ChessService.java
+++ src/main/java/io/github/oliviercailloux/y2018/assisted_board_games/service/ChessService.java
@@ -12,7 +12,6 @@ import javax.transaction.Transactional;
 import io.github.oliviercailloux.y2018.assisted_board_games.model.*;
 import io.github.oliviercailloux.y2018.assisted_board_games.utils.QueryHelper;
 
-
 @RequestScoped
 public class ChessService {
 	/***
@@ -25,51 +24,51 @@ public class ChessService {
 
 	@Inject
 	private QueryHelper helper;
-	
+
 	@Transactional
 	public List<ChessGameEntity> getAllGames() {
 		return em.createQuery(helper.selectAll(ChessGameEntity.class)).getResultList();
 	}
-	
+
 	@Transactional
 	public List<ChessMoveEntity> getAllMoves() {
 		return em.createQuery(helper.selectAll(ChessMoveEntity.class)).getResultList();
 	}
-	
-	@Transactional 
+
+	@Transactional
 	public ChessGameEntity getGame(int idGame) {
-		Query q = em.createNamedQuery("ChessGameEntity.find").setParameter("id", idGame); 
+		Query q = em.createNamedQuery("ChessGameEntity.find").setParameter("id", idGame);
 		return (ChessGameEntity) q.getSingleResult();
 	}
-	
-	@Transactional 
-	public int getLastGameId() {		
+
+	@Transactional
+	public int getLastGameId() {
 		Query q = em.createNamedQuery("ChessGameEntity.getLastGameId");
-		return  (Integer)q.getSingleResult();
+		return (Integer) q.getSingleResult();
 	}
-	
-	@Transactional 
+
+	@Transactional
 	public ChessMoveEntity getMove(int idMove) {
-		Query q = em.createNamedQuery("ChessMoveEntity.find").setParameter("id", idMove); 
+		Query q = em.createNamedQuery("ChessMoveEntity.find").setParameter("id", idMove);
 		return (ChessMoveEntity) q.getSingleResult();
 	}
+
 	/**
-	 * 
+	 * Gets the id of the last move played for the given game
 	 * @param idGame
 	 * @return id of the last Move played for the given game
 	 */
-	@Transactional 
-	public int getLastMoveId(int idGame) {		
-		Query q = em.createNamedQuery("ChessMoveEntity.getLastMoveId").setParameter("id", idGame);;
-		return  (Integer)q.getSingleResult();
+	@Transactional
+	public int getLastMoveId(int idGame) {
+		Query q = em.createNamedQuery("ChessMoveEntity.getLastMoveId").setParameter("id", idGame);
+		return (Integer) q.getSingleResult();
 	}
+
 	@Transactional
 	public void persist(ChessGameEntity game) {
 		em.persist(game);
 	}
-	
-	
-	
+
 	@Transactional
 	public void persist(ChessMoveEntity move) {
 		em.persist(move);
--- src/main/java/io/github/oliviercailloux/y2018/assisted_board_games/servlets/HelpServlet.java
+++ src/main/java/io/github/oliviercailloux/y2018/assisted_board_games/servlets/HelpServlet.java
@@ -1,7 +1,9 @@
 package io.github.oliviercailloux.y2018.assisted_board_games.servlets;
 
+import java.util.List;
 import java.util.logging.Logger;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -10,27 +12,38 @@ import javax.ws.rs.core.MediaType;
 
 import com.github.bhlangonijr.chesslib.Board;
 import com.github.bhlangonijr.chesslib.move.MoveConversionException;
+import com.github.bhlangonijr.chesslib.move.MoveException;
 import com.github.bhlangonijr.chesslib.move.MoveGenerator;
 import com.github.bhlangonijr.chesslib.move.MoveGeneratorException;
 import com.github.bhlangonijr.chesslib.move.MoveList;
+
+import io.github.oliviercailloux.y2018.assisted_board_games.model.ChessGameEntity;
+import io.github.oliviercailloux.y2018.assisted_board_games.model.ChessMoveEntity;
+import io.github.oliviercailloux.y2018.assisted_board_games.service.ChessService;
+import io.github.oliviercailloux.y2018.assisted_board_games.utils.GameHelper;
 
 /***
  * 
  * @author Megan Brassard
  *
  */
-@Path("help")
+@Path("/help")
 public class HelpServlet {
 
 	private static final Logger LOGGER = Logger.getLogger(HelpServlet.class.getCanonicalName());
 
+	@Inject
+	private ChessService chessS;
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
-	public String suggestMove(@QueryParam("state") String s)
-			throws MoveGeneratorException, MoveConversionException {
-		LOGGER.info("Request GET on HelpServlet with state :" + s);
-		Board b = new Board();
-		b.loadFromFen(s);
+	public String suggestMove(@QueryParam("game") int idGame)
+			throws MoveGeneratorException, MoveConversionException, MoveException {
+		
+		LOGGER.info("Request GET on HelpServlet with state :" + idGame);
+		System.out.println("suggesssst");
+		ChessGameEntity game= chessS.getGame(idGame);
+		List<ChessMoveEntity> moves=game.getMoves();
+		Board b= GameHelper.playMoves(moves);
 		// Generate possible moves
 		final MoveList l = MoveGenerator.generateLegalMoves(b);
 		return l.toSan();
