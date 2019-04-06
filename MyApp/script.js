bc4609b51f111b2f04219003e3ff18c3286d02b4
LoatR <m1105.111.dell@gmail.com>
2019-04-06 10:58:56 +0200

    add createNewGame request + get game request

--- MyApp/script.js
+++ MyApp/script.js
@@ -1,8 +1,13 @@
+/*** Global Vars ***/
 var idGame;
 
 function createNewBoard() {
-    idGame = servletCreateGame();
-    document.getElementById("board").innerHTML = servletGetGame(idGame);
+    idGame = createNewGameReq().then(function(res){
+        idGame = res;
+        getGameReq(idGame).then(function(res){
+            document.getElementById("board").innerHTML = res;
+        });
+    });
 }
 
 function getSuggestedMoves() {
@@ -20,3 +25,44 @@ function loadGame() {
 	idGame = document.getElementById("idGame");
     document.getElementById("board").innerHTML = servletGetGame(idGame);
 }
+
+/********* REQUEST PART ***********/
+function createNewGameReq () {
+      return fetch("http://localhost:8080/mychessgame/v1/game", {
+        method: "GET",
+        mode: "cors",
+        cache: "no-cache",
+        headers: {
+            "Content-Type": "text/plain",
+        },
+        redirect: "follow",
+        referrer: "no-referrer",
+        //body: JSON.stringify(data), // body data type must match "Content-Type" header
+    })
+        .then(response => response.text())
+        .then(function(response){
+            return response.toString();
+        }).catch(function (error){
+            console.log("An error occurend : ", error);
+        });
+}
+
+function getGameReq(idGame){
+    return fetch("http://localhost:8080/mychessgame/v1/game/getGame?game="+idGame, {
+        method: "GET",
+        mode: "cors",
+        cache: "no-cache",
+        headers: {
+            "Content-Type": "text/plain",
+        },
+        redirect: "follow",
+        referrer: "no-referrer",
+    })
+        .then(response => response.text())
+        .then(function(response){
+            return response.toString();
+        }).catch(function (error){
+            console.log("An error occurend : ", error);
+        });
+}
+
