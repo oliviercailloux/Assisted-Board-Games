554908fa3322d7120351c966782d5fb98be0e042
Spriithy <theo.dano@gmail.com>
2019-05-15 22:34:20 +0200

    Refactor 'Servlets' as Resources

---
+++ src/main/java/io/github/oliviercailloux/assisted_board_games/resources/GameResource.java
@@ -0,0 +1,110 @@
+package io.github.oliviercailloux.assisted_board_games.resources;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.json.JsonObject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.github.bhlangonijr.chesslib.Board;
+import com.github.bhlangonijr.chesslib.Square;
+import com.github.bhlangonijr.chesslib.move.MoveException;
+
+import io.github.oliviercailloux.assisted_board_games.game.ChessMove;
+import io.github.oliviercailloux.assisted_board_games.model.GameEntity;
+import io.github.oliviercailloux.assisted_board_games.model.MoveEntity;
+import io.github.oliviercailloux.assisted_board_games.service.ChessService;
+import io.github.oliviercailloux.assisted_board_games.utils.GameHelper;
+
+@Path("game")
+@RequestScoped
+public class GameResource {
+
+    private static final Logger LOGGER = Logger.getLogger(GameResource.class.getCanonicalName());
+    @Inject
+    ChessService chessService;
+
+    @GET
+    @Path("new")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String createGame() {
+        LOGGER.info("Request GET on GameServlet : Adding a new game");
+        GameEntity game = new GameEntity();
+        chessService.persist(game);
+        return String.valueOf(game.getId());
+    }
+
+    @GET
+    @Path("get")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getGame(@QueryParam("gid") int gameId) throws MoveException {
+        LOGGER.info("Request GET on GameServlet : Returning game :" + gameId);
+        GameEntity game = chessService.getGame(gameId);
+        List<MoveEntity> moves = game.getMoves();
+        Board b = GameHelper.playMoves(moves);
+        return b.toString();
+    }
+
+    @GET
+    @Path("move")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String addMove(@QueryParam("gid") int gameId, @QueryParam("from") Square from, @QueryParam("to") Square to)
+            throws MoveException {
+        LOGGER.info("Request POST on GameServlet : Adding a move to game :" + gameId + " with from = " + from
+                + " with to = " + to);
+        GameEntity game = chessService.getGame(gameId);
+        MoveEntity move = new MoveEntity(from, to);
+        game.addMove(move);
+        List<MoveEntity> moves = game.getMoves();
+        Board b = GameHelper.playMoves(moves);
+        chessService.persist(move);
+        return b.toString();
+    }
+
+    @POST
+    @Path("move/json")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response addMove(@QueryParam("gid") int gameId, JsonObject jsonMove) {
+        LOGGER.info("Request POST on StateServlet : Adding a move");
+        final MoveEntity move = MoveEntity.fromMove(ChessMove.decode(jsonMove));
+        GameEntity game = chessService.getGame(gameId);
+        game.addMove(move);
+        chessService.persist(move);
+        return Response.ok().build();
+    }
+
+    @GET
+    @Path("moves")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getMoves(@QueryParam("gid") int gameId) {
+        LOGGER.info("Request GET on GameServlet : Returning moves for game :" + gameId);
+        List<MoveEntity> moves = chessService.getGame(gameId).getMoves();
+        StringBuilder sb = new StringBuilder();
+        for (MoveEntity move : moves) {
+            if (sb.length() != 0) {
+                sb.append(", ");
+            }
+            sb.append(move);
+        }
+        return sb.toString();
+    }
+
+    @GET
+    @Path("moves/last")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getLastMove(@QueryParam("gid") int gameId) {
+        LOGGER.info("Request GET on GameServlet : Returning last move : for game :" + gameId);
+        int moveId = chessService.getLastMoveId(gameId);
+        MoveEntity move = chessService.getMove(moveId);
+        return move.toString();
+    }
+}
--- src/main/java/io/github/oliviercailloux/assisted_board_games/resources/HelpResource.java
+++ src/main/java/io/github/oliviercailloux/assisted_board_games/resources/HelpResource.java
@@ -1,4 +1,4 @@
-package io.github.oliviercailloux.assisted_board_games.servlets;
+package io.github.oliviercailloux.assisted_board_games.resources;
 
 import java.util.List;
 import java.util.logging.Logger;
@@ -13,7 +13,6 @@ import javax.ws.rs.core.MediaType;
 
 import com.github.bhlangonijr.chesslib.Board;
 import com.github.bhlangonijr.chesslib.move.Move;
-import com.github.bhlangonijr.chesslib.move.MoveConversionException;
 import com.github.bhlangonijr.chesslib.move.MoveException;
 import com.github.bhlangonijr.chesslib.move.MoveGenerator;
 import com.github.bhlangonijr.chesslib.move.MoveGeneratorException;
@@ -31,38 +30,37 @@ import io.github.oliviercailloux.assisted_board_games.utils.GameHelper;
  */
 @Path("/help")
 @RequestScoped
-public class HelpServlet {
+public class HelpResource {
 
-    private static final Logger LOGGER = Logger.getLogger(HelpServlet.class.getCanonicalName());
+    private static final Logger LOGGER = Logger.getLogger(HelpResource.class.getCanonicalName());
     @Inject
-    ChessService chessS;
+    ChessService chessService;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String suggestMove(@QueryParam("game") int idGame)
-            throws MoveGeneratorException, MoveConversionException, MoveException {
-        LOGGER.info("Request GET on HelpServlet with state :" + idGame);
-        GameEntity game = chessS.getGame(idGame);
+    public String suggestMove(@QueryParam("gid") int gameId) throws MoveGeneratorException, MoveException {
+        LOGGER.info("Request GET on HelpServlet with state :" + gameId);
+        GameEntity game = chessService.getGame(gameId);
         List<MoveEntity> moves = game.getMoves();
-        Board b = GameHelper.playMoves(moves);
+        Board board = GameHelper.playMoves(moves);
         // Generate possible moves
-        final MoveList l = MoveGenerator.generateLegalMoves(b);
+        final MoveList moveList = MoveGenerator.generateLegalMoves(board);
         MoveList mated = new MoveList();
         MoveList staleMate = new MoveList();
-        for (Move m : l) {
-            b.doMove(m);
-            if (b.isMated()) {
-                mated.add(m);
-                l.remove(m);
+        for (Move move : moveList) {
+            board.doMove(move);
+            if (board.isMated()) {
+                mated.add(move);
+                moveList.remove(move);
             }
-            if (b.isStaleMate()) {
-                staleMate.add(m);
-                l.remove(m);
+            if (board.isStaleMate()) {
+                staleMate.add(move);
+                moveList.remove(move);
             }
-            b.undoMove();
+            board.undoMove();
         }
         return "Move to mate opponent : " + mated.toString() + "\n"
                 + "Move leading to stalemate : " + staleMate.toString() + "\n"
-                + "Other legal move : " + l.toString();
+                + "Other legal move : " + moveList.toString();
     }
 }
