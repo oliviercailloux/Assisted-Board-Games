57a443dec0f0fe61b59e8997dd097d093f44490a
MarylineChen <maryline.chen98@gmail.com>
2019-06-02 21:16:00 +0200

    Fix time computation routines
    Correctly manage real time
    
    Co-Authored-By: Théophile Dano <theo.dano@gmail.com>

--- src/main/java/io/github/oliviercailloux/assisted_board_games/utils/ClockUtils.java
+++ src/main/java/io/github/oliviercailloux/assisted_board_games/utils/ClockUtils.java
@@ -28,13 +28,19 @@ public class ClockUtils {
             // the real clock time
             moveTimes.add(LocalTime.now());
         }
-        Duration blackMoveDuration;
-        Duration leftBlackDuration = game.getDuration();
-        for (int i = 1; i < moveTimes.size(); i += 2) { // Si <= 1 élément => ok on rentre pas dans la boucle
-            blackMoveDuration = Duration.between(moveTimes.get(i - 1), moveTimes.get(i));
-            leftBlackDuration = leftBlackDuration.minus(blackMoveDuration).plus(game.getClockIncrement());
+        Duration timeLeft = game.getDuration();
+        for (int i = 0; i < moveTimes.size() - 1; i += 2) {
+            LocalTime whiteTime = moveTimes.get(i);
+            LocalTime blackTime = moveTimes.get(i + 1); // this works because we made sure the list contained an odd
+                                                        // amount of moves by adding a virtual one
+            Duration moveDuration = Duration.between(whiteTime, blackTime);
+            timeLeft = timeLeft.minus(moveDuration);
+            // increment only when the move is not virtual (i.e. it is black's turn)
+            if (i != moves.size() - 1) {
+                timeLeft = timeLeft.plus(game.getClockIncrement());
+            }
         }
-        return leftBlackDuration;
+        return timeLeft;
     }
 
     public static Duration getWhiteRemainingDuration(GameEntity game) {
@@ -42,21 +48,27 @@ public class ClockUtils {
         List<LocalTime> moveTimes = moves.stream()
                         .map(MoveEntity::getTime)
                         .collect(Collectors.toList());
+        Duration timeLeft = game.getDuration();
+        if (moves.isEmpty()) {
+            // White is playing his first move
+            Duration timeElapsed = Duration.between(game.getStartTime(), LocalTime.now());
+            return timeLeft.minus(timeElapsed);
+        }
         if (moves.size() % 2 == 0) {
             // in this case, it is white turn and we need to add a virtual move time to get
             // the real clock time
             moveTimes.add(LocalTime.now());
         }
-        Duration whiteMoveDuration;
-        Duration leftWhiteDuration = game.getDuration();
-        if (!moves.isEmpty()) {
-            whiteMoveDuration = Duration.between(game.getStartTime(), moveTimes.get(0));
-            leftWhiteDuration = leftWhiteDuration.minus(whiteMoveDuration).plus(game.getClockIncrement());
+        for (int i = 0; i < moveTimes.size(); i += 2) {
+            LocalTime whiteTime = moveTimes.get(i);
+            LocalTime blackTime = i == 0 ? game.getStartTime() : moveTimes.get(i - 1);
+            Duration moveDuration = Duration.between(blackTime, whiteTime);
+            timeLeft = timeLeft.minus(moveDuration);
+            // increment only when the move is not virtual (i.e. it is white's turn)
+            if (i != moves.size()) {
+                timeLeft = timeLeft.plus(game.getClockIncrement());
+            }
         }
-        for (int i = 2; i < moves.size(); i += 2) { // on entre dans la boucle que si y a 3 éléments
-            whiteMoveDuration = Duration.between(moveTimes.get(i - 1), moveTimes.get(i));
-            leftWhiteDuration = leftWhiteDuration.minus(whiteMoveDuration).plus(game.getClockIncrement());
-        }
-        return leftWhiteDuration;
+        return timeLeft;
     }
 }
