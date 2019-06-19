79a3c4e9e1c5072635e3f1ac05d67a726adb7a2d
Spriithy <theo.dano@gmail.com>
2019-06-19 19:27:03 +0100

    Update GameStateTest.java

--- src/test/java/io/github/oliviercailloux/assisted_board_games/model/GameStateTest.java
+++ src/test/java/io/github/oliviercailloux/assisted_board_games/model/GameStateTest.java
@@ -7,9 +7,7 @@ import java.time.Instant;
 
 import org.junit.jupiter.api.Test;
 
-import com.github.bhlangonijr.chesslib.Piece;
 import com.github.bhlangonijr.chesslib.Side;
-import com.github.bhlangonijr.chesslib.Square;
 
 import io.github.oliviercailloux.assisted_board_games.model.state.GameState;
 import io.github.oliviercailloux.assisted_board_games.model.state.PlayerState;
@@ -31,9 +29,5 @@ class GameStateTest {
         assertEquals(gameEntity.getClockDuration(), white.getRemainingTime());
         PlayerState black = gameState.getPlayerState(Side.BLACK);
         assertEquals(gameEntity.getClockDuration(), black.getRemainingTime());
-        //
-        MoveDAO moveDAO = MoveDAO.createMoveDAO(Square.E4, Square.E2, Piece.NONE);
-        MoveEntity moveEntity = MoveEntity.createMoveEntity(gameEntity, moveDAO, Duration.ofSeconds(8));
-        gameEntity.addMove(moveEntity);
     }
 }
