ff2fd29ba4f4c126729ccb4c8e0cbcc7a41351af
marielou-estimeo <marielou@estimeo.com>
2022-05-25 16:11:07 +0200

    tests for gameEntity and ChechBoard

---
+++ src/main/java/io/github/oliviercailloux/sample_quarkus_heroku/ChessBoardServiceTest.java
@@ -0,0 +1,31 @@
+package io.github.oliviercailloux.sample_quarkus_heroku;
+
+import java.util.List;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RequestScoped
+public class ChessBoardServiceTest {
+	@SuppressWarnings("unused")
+	private static final Logger LOGGER = LoggerFactory.getLogger(ChessBoardServiceTest.class);
+
+	@Inject
+	EntityManager em;
+
+	@Inject
+	QueryHelper helper;
+
+	@Transactional
+	public List<ChessBoard> getAll() {
+		return em.createQuery(helper.selectAll(ChessBoard.class)).getResultList();
+	}
+
+	@Transactional
+	public void persist(ChessBoard chessboard) {
+		em.persist(chessboard);
+	}
+}
---
+++ src/main/java/io/github/oliviercailloux/sample_quarkus_heroku/GameServiceTest.java
@@ -0,0 +1,31 @@
+package io.github.oliviercailloux.sample_quarkus_heroku;
+
+import java.util.List;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RequestScoped
+public class GameServiceTest {
+	@SuppressWarnings("unused")
+	private static final Logger LOGGER = LoggerFactory.getLogger(GameServiceTest.class);
+
+	@Inject
+	EntityManager em;
+
+	@Inject
+	QueryHelper helper;
+
+	@Transactional
+	public List<GameEntity> getAll() {
+		return em.createQuery(helper.selectAll(GameEntity.class)).getResultList();
+	}
+
+	@Transactional
+	public void persist(GameEntity game) {
+		em.persist(game);
+	}
+}
