d9f7bea90966160ed684594d39e3a7c40229955b
marielou-estimeo <marielou@estimeo.com>
2022-06-10 19:43:18 +0200

    change redirection

--- src/main/resources/META-INF/resources/js/index.js
+++ src/main/resources/META-INF/resources/js/index.js
@@ -1,6 +1,6 @@
 // Triggers a function after a button click, here we are going to redirect the user to a selected game page
 document.getElementById("Chess").onclick = () => {
-    window.location.assign("Chess.html");
+    window.location.assign("chess.html");
 };
 
 /*
