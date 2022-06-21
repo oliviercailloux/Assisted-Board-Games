b2c7acd9d22511dc7ffbb67ea945179d12a082d0
aminekebouche <keboucheamine6@gmail.com>
2022-06-21 17:00:52 +0200

    form

--- src/main/java/io/github/oliviercailloux/abg/ChessBoard.java
+++ src/main/java/io/github/oliviercailloux/abg/ChessBoard.java
@@ -19,71 +19,71 @@ import javax.persistence.Table;
 @Entity
 public class ChessBoard extends MyBoard {
 
-    public static final ChessBoard STARTING_CHESS_BOARD = new ChessBoard();
+  public static final ChessBoard STARTING_CHESS_BOARD = new ChessBoard();
 
-    String fen;
-    Side sideToMove;
+  String fen;
+  Side sideToMove;
 
-    ChessBoard() {
-	this.fen = GameEntity.STARTING_FEN_POSITION;
-	this.sideToMove = Side.WHITE;
+  ChessBoard() {
+    this.fen = GameEntity.STARTING_FEN_POSITION;
+    this.sideToMove = Side.WHITE;
+  }
+
+  private ChessBoard(Board board) {
+    checkNotNull(board);
+    this.fen = board.getFen(true);
+    this.sideToMove = board.getSideToMove();
+  }
+
+  public static ChessBoard createChessBoard() {
+    return new ChessBoard();
+  }
+
+  public static ChessBoard createChessBoard(String fen) {
+    final Board board = new Board();
+    board.loadFromFen(fen);
+    return new ChessBoard(board);
+  }
+
+  public static ChessBoard createChessBoard(Board board) {
+    return new ChessBoard(board);
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getFen() {
+    return fen;
+  }
+
+  @Override
+  public Side getSideToMove() {
+    return sideToMove;
+  }
+
+  public ChessBoard doMove(MoveEntity move) throws MoveException {
+    final Board board = asBoard();
+    if (!board.doMove(move.asMove(), true)) {
+      throw new MoveException();
     }
+    return createChessBoard(board);
+  }
 
-    private ChessBoard(Board board) {
-	checkNotNull(board);
-	this.fen = board.getFen(true);
-	this.sideToMove = board.getSideToMove();
+  @Override
+  public ChessBoard doMoves(List<MoveEntity> moves) throws MoveException {
+    final Board board = asBoard();
+    for (MoveEntity move : moves) {
+      if (!board.doMove(move.asMove(), true)) {
+        throw new MoveException();
+      }
     }
+    return createChessBoard(board);
+  }
 
-    public static ChessBoard createChessBoard() {
-	return new ChessBoard();
-    }
-
-    public static ChessBoard createChessBoard(String fen) {
-	final Board board = new Board();
-	board.loadFromFen(fen);
-	return new ChessBoard(board);
-    }
-
-    public static ChessBoard createChessBoard(Board board) {
-	return new ChessBoard(board);
-    }
-
-    public int getId() {
-	return id;
-    }
-
-    public String getFen() {
-	return fen;
-    }
-
-    @Override
-    public Side getSideToMove() {
-	return sideToMove;
-    }
-
-    public ChessBoard doMove(MoveEntity move) throws MoveException {
-	final Board board = asBoard();
-	if (!board.doMove(move.asMove(), true)) {
-	    throw new MoveException();
-	}
-	return createChessBoard(board);
-    }
-
-    @Override
-    public ChessBoard doMoves(List<MoveEntity> moves) throws MoveException {
-	final Board board = asBoard();
-	for (MoveEntity move : moves) {
-	    if (!board.doMove(move.asMove(), true)) {
-		throw new MoveException();
-	    }
-	}
-	return createChessBoard(board);
-    }
-
-    public Board asBoard() {
-	final Board board = new Board();
-	board.loadFromFen(fen);
-	return board;
-    }
+  public Board asBoard() {
+    final Board board = new Board();
+    board.loadFromFen(fen);
+    return board;
+  }
 }
