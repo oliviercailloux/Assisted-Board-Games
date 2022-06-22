ff7bf3568797d38d63cf068c96ac7e769e377691
aminekebouche <keboucheamine6@gmail.com>
2022-06-22 17:20:51 +0200

    format

--- src/main/java/io/github/oliviercailloux/abg/QueryHelper.java
+++ src/main/java/io/github/oliviercailloux/abg/QueryHelper.java
@@ -9,15 +9,14 @@ import javax.persistence.criteria.Root;
 
 @ApplicationScoped
 public class QueryHelper {
-	@Inject
-	EntityManagerFactory emf;
+  @Inject
+  EntityManagerFactory emf;
 
-	public <T> CriteriaQuery<T> selectAll(Class<T> type) {
-		final CriteriaBuilder criteriaBuilder = emf.getCriteriaBuilder();
-		final CriteriaQuery<T> query = criteriaBuilder.createQuery(type);
-		final Root<T> from = query.from(type);
-		query.select(from);
-		return query;
-	}
-
+  public <T> CriteriaQuery<T> selectAll(Class<T> type) {
+    final CriteriaBuilder criteriaBuilder = emf.getCriteriaBuilder();
+    final CriteriaQuery<T> query = criteriaBuilder.createQuery(type);
+    final Root<T> from = query.from(type);
+    query.select(from);
+    return query;
+  }
 }
