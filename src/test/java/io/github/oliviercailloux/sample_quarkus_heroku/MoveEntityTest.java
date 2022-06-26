45e146e9ed95a1a9c29d1d7bdcb02b2f1574f06b
willytran <willy.tran@dauphine.eu>
2022-06-26 22:12:20 +0200

    a

--- src/test/java/io/github/oliviercailloux/sample_quarkus_heroku/MoveEntityTest.java
+++ src/test/java/io/github/oliviercailloux/sample_quarkus_heroku/MoveEntityTest.java
@@ -6,54 +6,60 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.github.bhlangonijr.chesslib.Piece;
 import com.github.bhlangonijr.chesslib.Square;
+import com.github.bhlangonijr.chesslib.move.Move;
 import io.github.oliviercailloux.abg.GameEntity;
 import io.github.oliviercailloux.abg.MoveDAO;
 import io.github.oliviercailloux.abg.MoveEntity;
 import io.quarkus.runtime.ApplicationConfig;
 import io.quarkus.test.junit.QuarkusTest;
 import java.time.Duration;
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+import javax.json.bind.JsonbConfig;
+import javax.json.bind.annotation.JsonbProperty;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @QuarkusTest
 public class MoveEntityTest {
   static MoveEntity moveEntity;
-
   GameEntity game;
-  MoveDAO move;
+  static MoveDAO move;
   Duration duration;
-
-  Square from;
-  Square to;
-  Piece promotion;
-
-  @Test
-  // Test for the method MoveEntity(GameEntity gameEntity, Square from, Square to, Piece promotion,
-  // Duration duration)
-  public void testMoveEntityMethod() {
-    game = new GameEntity();
-    // Test de la méthode en laissant les paramètres promotion et duration à NULL
-    moveEntity = new MoveEntity(game, Square.A7, Square.B3, promotion, duration);
-    assertNotNull(moveEntity.getGame());
-    assertEquals(Duration.ZERO, moveEntity.getDuration());
-    assertEquals(Piece.NONE, moveEntity.getPromotion());
-
-    // Test de la méthode en instanciant les paramètres promotion et duration)
-    moveEntity =
-        new MoveEntity(game, Square.A7, Square.B3, Piece.BLACK_KING, Duration.ofSeconds(50));
-    assertNotNull(moveEntity.getGame());
-    assertEquals(Duration.ofSeconds(50), moveEntity.getDuration());
-    assertEquals(Piece.BLACK_KING, moveEntity.getPromotion());
-  }
+  static Square from;
+  static Square to;
+  static Piece promotion;
+  private static final Logger LOGGER = LoggerFactory.getLogger(MoveEntityTest.class);
 
   @Test
-  public void testCreateMoveEntity() {
+  public void testCreateMoveEntityNull() {
     // Test
     assertThrows(IllegalArgumentException.class, () -> {
       MoveEntity.createMoveEntity(game, move, duration);
     });
-    move = new MoveDAO(Square.A7, Square.B3, Piece.BLACK_KING);
-    moveEntity =
-        new MoveEntity(game, Square.A7, Square.B3, Piece.BLACK_KING, Duration.ofSeconds(50));
+    LOGGER.info("The Illegal Exception has been thrown and detected");
+  }
+
+  @Test
+  public void testCreateMoveEntityInstance() {
+    from = Square.A7;
+    to = Square.B3;
+    promotion = Piece.BLACK_KING;
+    game = new GameEntity();
+    move = MoveDAO.createMoveDAO(from, to, promotion);
+    duration = Duration.ofSeconds(50);
+
+    moveEntity = MoveEntity.createMoveEntity(game, move, duration);
+    assertEquals(Duration.ofSeconds(1800), moveEntity.getGame().getClockDuration());
+    assertEquals(Duration.ofSeconds(10), moveEntity.getGame().getClockIncrement());
+    assertEquals(Duration.ofSeconds(1800), moveEntity.getGame().getClockDuration());
+    assertEquals(GameEntity.STARTING_FEN_POSITION, moveEntity.getGame().getStartBoard().getFen());
+    assertEquals(Square.A7, moveEntity.getFrom());
+    assertEquals(Square.B3, moveEntity.getTo());
+    assertEquals(Piece.BLACK_KING, moveEntity.getPromotion());
+    LOGGER.info("The Move entity has been successfully instantiated");
+
   }
 }
