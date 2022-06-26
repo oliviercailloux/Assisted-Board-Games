943cb1e41b4621bd0f2f6e694ea0b7f480030b1e
aminekebouche <keboucheamine6@gmail.com>
2022-06-26 23:43:36 +0200

    CheckerBoard

--- src/main/java/io/github/oliviercailloux/abg/MyBoard.java
+++ src/main/java/io/github/oliviercailloux/abg/MyBoard.java
@@ -6,8 +6,6 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
 
 @Entity
 public abstract class MyBoard {
@@ -19,6 +17,8 @@ public abstract class MyBoard {
   String fen;
 
   public abstract Side getSideToMove();
+
   public abstract MyBoard doMoves(List<MoveEntity> moves) throws MoveException;
+
   public abstract String getFen();
 }
