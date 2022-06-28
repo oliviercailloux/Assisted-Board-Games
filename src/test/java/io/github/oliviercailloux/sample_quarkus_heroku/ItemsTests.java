384f8cf7f99c8a5204675e85748f60edde7fdbb5
willytran <willy.tran@dauphine.eu>
2022-06-28 21:58:14 +0200

    Revert "test"
    This reverts commit f601ce2e5e9116af37470d77e5cdc6f78dc89c96.

--- src/test/java/io/github/oliviercailloux/sample_quarkus_heroku/ItemsTests.java
+++ src/test/java/io/github/oliviercailloux/sample_quarkus_heroku/ItemsTests.java
@@ -10,18 +10,18 @@ import org.junit.jupiter.api.Test;
 @QuarkusTest
 public class ItemsTests {
 
-  @Test
-  public void testGet() {
-    given().when().get("/v0/items").then().statusCode(200).body(Is.is(""));
-  }
+	@Test
+	public void testGet() {
+		given().when().get("/v0/items").then().statusCode(200).body(Is.is(""));
+	}
 
-  @Test
-  public void testPost() {
-    given().post("/v0/items");
-    given().when().get("/v0/items").then().statusCode(200)
-        .body(MatchesPattern.matchesPattern("MyItem dated .*"));
-    given().post("/v0/items");
-    given().when().get("/v0/items").then().statusCode(200)
-        .body(MatchesPattern.matchesPattern("MyItem dated .*\nMyItem dated .*"));
-  }
+	@Test
+	public void testPost() {
+		given().post("/v0/items");
+		given().when().get("/v0/items").then().statusCode(200).body(MatchesPattern.matchesPattern("MyItem dated .*"));
+		given().post("/v0/items");
+		given().when().get("/v0/items").then().statusCode(200)
+				.body(MatchesPattern.matchesPattern("MyItem dated .*\nMyItem dated .*"));
+	}
+
 }
--- src/test/java/io/github/oliviercailloux/sample_quarkus_heroku/Unitest.java
+++ src/test/java/io/github/oliviercailloux/sample_quarkus_heroku/Unitest.java
@@ -16,32 +16,29 @@ import org.junit.jupiter.api.TestMethodOrder;
 @QuarkusTest
 @TestMethodOrder(OrderAnnotation.class)
 public class Unitest {
-  Duration reminTime = Duration.ofSeconds(1800);
+        Duration reminTime = Duration.ofSeconds(1800);
+	@Test
+	@Order(1)
+	public void testPostcreateGame() {
+		given().when().post("/v0/api/v1/game/new").then().statusCode(200).body(Is.is("1"));
+	}
 
-  @Test
-  @Order(1)
-  public void testPostcreateGame() {
-    given().when().post("/v0/api/v1/game/new").then().statusCode(200).body(Is.is("1"));
-  }
-
-  @Test
-  @Order(2)
-  public void testGetgetGame() {
-    given().when().get("/v0/api/v1/game/1").then().statusCode(200).body(
-        MatchesPattern.matchesPattern("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"));
-  }
-
-  // @Test
-  // @Order(3)
-  // public void testWilly(){
-  // given().contentType(ContentType.JSON).body(obj)
-  // .post("/v0/api/v1/game/1/move")
-  // .then().statusCode(200).body(Is.is(""));
-  // }
-  @Test
-  @Order(3)
-  public void testgetRemainingTime() {
-    given().when().get("/v0/api/v1/game/1/clock/black").then().statusCode(200)
-        .body(Is.is(reminTime));
-  }
+	@Test
+	@Order(2)
+	public void testGetgetGame() {
+		given().when().get("/v0/api/v1/game/1").then().statusCode(200).body(MatchesPattern.matchesPattern("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"));
+	}
+//	@Test
+//	@Order(3)
+//	public void testWilly(){
+//	    	given().contentType(ContentType.JSON).body(obj)
+//        .post("/v0/api/v1/game/1/move")
+//        .then().statusCode(200).body(Is.is(""));
+//	}	
+	@Test
+	@Order(3)
+	public void testgetRemainingTime(){
+	   given().when().get("/v0/api/v1/game/1/clock/black").then().statusCode(200).body(Is.is(reminTime));
+	}
 }
+
