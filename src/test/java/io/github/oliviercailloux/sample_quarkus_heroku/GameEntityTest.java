d11d1c75b4612328b347c61e6bdc821e6ac2647f
willytran <willy.tran@dauphine.eu>
2022-06-28 21:55:08 +0200

    Revert "a"
    This reverts commit 45e146e9ed95a1a9c29d1d7bdcb02b2f1574f06b.

--- src/test/java/io/github/oliviercailloux/sample_quarkus_heroku/GameEntityTest.java
+++ src/test/java/io/github/oliviercailloux/sample_quarkus_heroku/GameEntityTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import io.github.oliviercailloux.abg.ChessBoard;
 import io.github.oliviercailloux.abg.GameEntity;
-import io.github.oliviercailloux.abg.model.state.GameState;
 import io.quarkus.test.junit.QuarkusTest;
 import java.time.Clock;
 import java.time.Duration;
@@ -24,25 +23,15 @@ import org.slf4j.LoggerFactory;
 public class GameEntityTest {
 
   static GameEntity game;
-  static GameState gameState;
-  final Instant fixedInstant =
-      Instant.now(Clock.fixed(Instant.parse("2022-06-22T10:00:00Z"), ZoneOffset.UTC));
   private static final Logger LOGGER = LoggerFactory.getLogger(GameEntityTest.class);
+
+  @BeforeAll
+  public static void instanciate() {
+    game = new GameEntity();
+  }
 
   @Test
   public void testEntity() { // Test du constructeur de GameEntity
-    game = new GameEntity();
-    assertEquals(Duration.ofSeconds(1800), game.getClockDuration());
-    assertEquals(Duration.ofSeconds(10), game.getClockIncrement());
-    ArrayList moves = new ArrayList<>(); // avoid NPE in tests
-    assertEquals(moves, game.getMoves());
-    assertEquals(GameEntity.STARTING_FEN_POSITION, game.getStartBoard().getFen());
-    LOGGER.info("The game entity has been successfully instantiated");
-  }  
-  
-  @Test
-  public void testGameEntity() { // Test du constructeur de GameEntity
-    game = new GameEntity(gameState, Instant.);
     assertEquals(Duration.ofSeconds(1800), game.getClockDuration());
     assertEquals(Duration.ofSeconds(10), game.getClockIncrement());
     ArrayList moves = new ArrayList<>(); // avoid NPE in tests
@@ -53,7 +42,9 @@ public class GameEntityTest {
 
   @Test
   public void testSetStartTime() {
-
+    final Instant fixedInstant =
+        Instant.now(Clock.fixed(Instant.parse("2022-06-22T10:00:00Z"), ZoneOffset.UTC));
+    game.setStartTime(fixedInstant);
     assertNotNull(game.getStartTime());
     assertEquals(fixedInstant, game.getStartTime());
     LOGGER.info("The start time has been successfully instantiated");
@@ -62,10 +53,7 @@ public class GameEntityTest {
 
   @Test
   public void testcomputeRemainingTime() {
-   
-    game.setStartTime(fixedInstant);
-    assertNotNull(game.getStartTime());
-    assertEquals(fixedInstant, game.getStartTime());
+
   }
 }
 
